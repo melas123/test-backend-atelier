fabf0220f3e11205e5f8128bc46c437e3f1e2b34
Salem <salemharrathi@gmail.com>
2024-04-04 21:51:49 +0200

    get statistics

--- src/resource/players/players.controller.ts
+++ src/resource/players/players.controller.ts
@@ -1,17 +1,6 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-  UseFilters,
-} from '@nestjs/common';
+import { Controller, Delete, Get, Param, UseFilters } from '@nestjs/common';
 import { ApiOperation, ApiParam, ApiResponse } from '@nestjs/swagger';
 import { HttpExceptionFilter } from 'src/http-exception.filter';
-import { CreatePlayerDto } from './dto/create-player.dto';
-import { UpdatePlayerDto } from './dto/update-player.dto';
 import { PlayersService } from './players.service';
 import { FindOneParams } from './validators/findOne.validator';
 
@@ -20,15 +9,16 @@ import { FindOneParams } from './validators/findOne.validator';
 export class PlayersController {
   constructor(private readonly playersService: PlayersService) {}
 
-  @Post()
-  create(@Body() createPlayerDto: CreatePlayerDto) {
-    return this.playersService.create(createPlayerDto);
-  }
-
   @Get()
   @ApiOperation({ summary: 'Get list of players ordered by rank' })
   findAll() {
     return this.playersService.findAll();
+  }
+
+  @Get('statistics')
+  @ApiOperation({ summary: 'Get some statistics' })
+  getStatistics() {
+    return this.playersService.getStatistics();
   }
 
   @Get(':id')
@@ -43,11 +33,6 @@ export class PlayersController {
   @ApiResponse({ status: 500, description: 'Internal server error.' })
   findOne(@Param() params: FindOneParams) {
     return this.playersService.findOne(+params.id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePlayerDto: UpdatePlayerDto) {
-    return this.playersService.update(+id, updatePlayerDto);
   }
 
   @Delete(':id')
--- src/resource/players/players.service.ts
+++ src/resource/players/players.service.ts
@@ -1,15 +1,8 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-
-import { CreatePlayerDto } from './dto/create-player.dto';
-import { UpdatePlayerDto } from './dto/update-player.dto';
 import { Player } from './entities/player.entity';
 
 @Injectable()
 export class PlayersService {
-  create(createPlayerDto: CreatePlayerDto) {
-    return 'This action adds a new player';
-  }
-
   findAll() {
     return Player.query(
       `Select * from player order by JSON_EXTRACT(data, '$.rank')`,
@@ -24,11 +17,61 @@ export class PlayersService {
     return player;
   }
 
-  update(id: number, updatePlayerDto: UpdatePlayerDto) {
-    return `This action updates a #${id} player`;
+  remove(id: number) {
+    Player.delete({ id });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} player`;
+  async getStatistics() {
+    const result = await Player.find();
+
+    return {
+      bestCountry: this.bestCountry(result),
+      averageDmi: this.dmiAverage(result),
+      medianSize: this.median(result),
+    };
+  }
+
+  bestCountry = (listPlayers) => {
+    const countries = [];
+    listPlayers.forEach((element) => {
+      countries.push({
+        country: element.country.code,
+        wins: this.calculateWins(element.data.last),
+      });
+    });
+    return countries.reduce(function (prev, current) {
+      return prev && prev.wins > current.wins ? prev : current;
+    });
+  };
+
+  dmiAverage = (listPlayers) => {
+    const total = listPlayers.reduce((acc, current) => {
+      const imc =
+        current.data.weight / 1000 / Math.pow(current.data.height / 100, 2);
+      return acc + imc;
+    }, 0);
+    return total / listPlayers.length;
+  };
+
+  calculateWins = (str) => {
+    let sum = 0;
+    for (let i = 0; i < str.length; i++) {
+      sum += parseInt(str[i]);
+    }
+    return sum;
+  };
+
+  median(elements) {
+    const medianHeightArr = elements.map((element) => element.data.height);
+
+    medianHeightArr.sort(function (a, b) {
+      return a - b;
+    });
+
+    const half = Math.floor(medianHeightArr.length / 2);
+
+    if (medianHeightArr.length % 2) return medianHeightArr[half];
+
+    return (medianHeightArr[half - 1] + medianHeightArr[half]) / 2.0;
   }
 }
