14e76fa44f8e0cbb7a205ac54632bdecb6fe82db
Salem <salemharrathi@gmail.com>
2024-04-06 17:02:00 +0200

    organize project

--- src/resource/players/players.service.ts
+++ src/resource/players/players.service.ts
@@ -1,21 +1,18 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { bestCountry, dmiAverage, median } from '../../utils/utils';
 import { Player } from './entities/player.entity';
 
 @Injectable()
 export class PlayersService {
-  constructor(
-    @InjectRepository(Player) private readonly playerRepo: Repository<Player>,
-  ) {}
+  constructor() {}
   findAll(): Promise<Player[]> {
-    return this.playerRepo.query(
+    return Player.query(
       `Select * from player order by JSON_EXTRACT(data, '$.rank')`,
     );
   }
 
   async findOne(id: number): Promise<Player> {
-    const player = await this.playerRepo.findOneBy({ id });
+    const player = await Player.findOneBy({ id });
     if (!player) {
       throw new NotFoundException('Player not found.');
     }
@@ -24,7 +21,7 @@ export class PlayersService {
 
   async remove(id: number): Promise<{ deleted: boolean; message?: string }> {
     try {
-      await this.playerRepo.delete({ id });
+      await Player.delete({ id });
       return { deleted: true };
     } catch (err) {
       return { deleted: false, message: err.message };
@@ -32,56 +29,12 @@ export class PlayersService {
   }
 
   async getStatistics() {
-    const result = await this.playerRepo.find();
+    const result = await Player.find();
 
     return {
-      bestCountry: this.bestCountry(result),
-      averageDmi: this.dmiAverage(result),
-      medianSize: this.median(result),
+      bestCountry: bestCountry(result),
+      averageDmi: dmiAverage(result),
+      medianSize: median(result),
     };
-  }
-
-  bestCountry = (listPlayers) => {
-    const countries = [];
-    listPlayers.forEach((element) => {
-      countries.push({
-        country: element.country.code,
-        wins: this.calculateWins(element.data.last),
-      });
-    });
-    return countries.reduce(function (prev, current) {
-      return prev && prev.wins > current.wins ? prev : current;
-    });
-  };
-
-  dmiAverage = (listPlayers) => {
-    const total = listPlayers.reduce((acc, current) => {
-      const imc =
-        current.data.weight / 1000 / Math.pow(current.data.height / 100, 2);
-      return acc + imc;
-    }, 0);
-    return total / listPlayers.length;
-  };
-
-  calculateWins = (str) => {
-    let sum = 0;
-    for (let i = 0; i < str.length; i++) {
-      sum += parseInt(str[i]);
-    }
-    return sum;
-  };
-
-  median(elements) {
-    const medianHeightArr = elements.map((element) => element.data.height);
-
-    medianHeightArr.sort(function (a, b) {
-      return a - b;
-    });
-
-    const half = Math.floor(medianHeightArr.length / 2);
-
-    if (medianHeightArr.length % 2) return medianHeightArr[half];
-
-    return (medianHeightArr[half - 1] + medianHeightArr[half]) / 2.0;
   }
 }
